f22e3ebc8f04aecaf1708432f92f2ee744920bb2
Jmestrelozano <jmestrelozano@gmail.com>
2025-05-27 17:32:32 -0500

    feat(routes): configure root and wildcard routes with lazy-loaded components
    Set up the main route to load LayoutComponent with WeddingPageComponent as the default child. Added wildcard route to redirect unknown paths to the root.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./main/layout/layout.component').then(m => m.LayoutComponent),
+    children: [
+      {
+        path: '',
+            loadComponent: () => import('./main/pages/wedding-page/wedding-page.component').then(m => m.WeddingPageComponent), 
+      },
+      // otras rutas hijas
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
