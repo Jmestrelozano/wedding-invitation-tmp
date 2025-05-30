2d6062b6f2e38ba5b6bf76f8ffd44e396a649673
Jmestrelozano <jmestrelozano@gmail.com>
2025-05-30 15:39:45 -0500

    feat(banner-home): create BannerHomeComponent with Lottie animation
    Implemented standalone BannerHomeComponent using ngx-lottie to display a looping, autoplaying arrow animation. Includes animationCreated handler and style/template references.

---
+++ src/app/components/wedding-components/banner-home/banner-home.component.ts
@@ -0,0 +1,27 @@
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import { Component } from '@angular/core';
+import player, { AnimationItem } from 'lottie-web';
+
+export function playerFactory() {
+  return player;
+}
+@Component({
+  selector: 'app-banner-home',
+  standalone: true,
+  imports: [LottieComponent],
+  templateUrl: './banner-home.component.html',
+  styleUrl: './banner-home.component.css'
+})
+export class BannerHomeComponent {
+  private animationItem: AnimationItem | undefined;
+
+  options: AnimationOptions = {
+    path: 'assets/animations/arrow_continue.json', // Ruta al archivo JSON
+    loop: true,
+    autoplay: true,
+  };
+
+  animationCreated(animationItem: AnimationItem): void {
+    this.animationItem = animationItem;
+  }
+}
