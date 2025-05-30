6ff358ed395ac46c300631b9c4609242c22197f2
Jmestrelozano <jmestrelozano@gmail.com>
2025-05-30 15:37:54 -0500

    feat(wedding-page): set up standalone WeddingPageComponent with BannerHome and EventDetails
    Configured WeddingPageComponent as a standalone Angular component importing BannerHomeComponent and EventDetailsComponent, and linked to its template.

--- src/app/main/pages/wedding-page/wedding-page.component.ts
+++ src/app/main/pages/wedding-page/wedding-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerHomeComponent } from "../../../components/wedding-components/banner-home/banner-home.component";
+import { EventDetailsComponent } from "../../../components/wedding-components/event-details/event-details.component";
 
 @Component({
   selector: 'app-wedding-page',
   standalone: true,
-  imports: [],
+  imports: [BannerHomeComponent, EventDetailsComponent],
   templateUrl: './wedding-page.component.html',
 })
 export class WeddingPageComponent {
