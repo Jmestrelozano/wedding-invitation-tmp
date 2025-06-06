ba8b467ad50edc1e95a20e98345814fff8cbf767
Jmestrelozano <jmestrelozano@gmail.com>
2025-06-06 14:23:12 -0500

    feat(wedding-page): add PortraitsComponent to wedding page imports
    Updated WeddingPageComponent to import and include PortraitsComponent, enabling the display of the portraits carousel on the wedding page.

--- src/app/main/pages/wedding-page/wedding-page.component.ts
+++ src/app/main/pages/wedding-page/wedding-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BannerHomeComponent } from "../../../components/wedding-components/banner-home/banner-home.component";
 import { WeddingCountdownComponent } from "../../../components/wedding-components/wedding-countdown/wedding-countdown.component";
 import { EventScheduleComponent } from "../../../components/wedding-components/event-schedule/event-schedule.component";
+import { PortraitsComponent } from "../../../components/wedding-components/portraits/portraits.component";
 
 @Component({
   selector: 'app-wedding-page',
   standalone: true,
-  imports: [BannerHomeComponent, WeddingCountdownComponent, EventScheduleComponent],
+  imports: [BannerHomeComponent, WeddingCountdownComponent, EventScheduleComponent, PortraitsComponent],
   templateUrl: './wedding-page.component.html',
 })
 export class WeddingPageComponent {
