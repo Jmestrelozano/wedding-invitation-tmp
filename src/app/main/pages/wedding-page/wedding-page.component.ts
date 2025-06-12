639b5d452b9d6814a422293f0edbc9e1786aca84
Jmestrelozano <jmestrelozano@gmail.com>
2025-06-11 21:37:09 -0500

    feat(wedding-page): add InstructionsComponent to wedding page imports
    Updated WeddingPageComponent to import and include InstructionsComponent, enabling the display of instructions on the wedding page.

--- src/app/main/pages/wedding-page/wedding-page.component.ts
+++ src/app/main/pages/wedding-page/wedding-page.component.ts
@@ -3,11 +3,12 @@ import { BannerHomeComponent } from "../../../components/wedding-components/bann
 import { WeddingCountdownComponent } from "../../../components/wedding-components/wedding-countdown/wedding-countdown.component";
 import { EventScheduleComponent } from "../../../components/wedding-components/event-schedule/event-schedule.component";
 import { PortraitsComponent } from "../../../components/wedding-components/portraits/portraits.component";
+import { InstructionsComponent } from "../../../components/wedding-components/instructions/instructions.component";
 
 @Component({
   selector: 'app-wedding-page',
   standalone: true,
-  imports: [BannerHomeComponent, WeddingCountdownComponent, EventScheduleComponent, PortraitsComponent],
+  imports: [BannerHomeComponent, WeddingCountdownComponent, EventScheduleComponent, PortraitsComponent, InstructionsComponent],
   templateUrl: './wedding-page.component.html',
 })
 export class WeddingPageComponent {
