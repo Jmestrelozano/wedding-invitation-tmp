c75ebb3e500efe8bdd9433c7e4836f156859522f
Jmestrelozano <jmestrelozano@gmail.com>
2025-06-29 14:55:09 -0500

    feat(confirmations): add modal visibility state and open/close methods
    Introduced isModalVisible property and openModal/closeModal methods to control the display of the confirmation modal in ConfirmationsComponent.

--- src/app/components/wedding-components/confirmations/confirmations.component.ts
+++ src/app/components/wedding-components/confirmations/confirmations.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { TitleCoupleComponent } from "../../ui/title-couple/title-couple.component";
+import { TitleCoupleComponent } from '../../ui/title-couple/title-couple.component';
+import { ModalComponent } from '../../common/modal/modal.component';
+import { ContentConfirmationModalComponent } from '../../contents/content-confirmation-modal/content-confirmation-modal.component';
 
 @Component({
   selector: 'app-confirmations',
   standalone: true,
-  imports: [TitleCoupleComponent],
+  imports: [
+    TitleCoupleComponent,
+    ModalComponent,
+    ContentConfirmationModalComponent,
+  ],
   templateUrl: './confirmations.component.html',
-  styleUrl: './confirmations.component.css'
+  styleUrl: './confirmations.component.css',
 })
 export class ConfirmationsComponent {
+  isModalVisible = false;
 
+  openModal() {
+    this.isModalVisible = true;
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
+  }
 }
